0684067dabd7ba211ffe0a8f240799e3fbb5ef88
Daniël Klabbers <daniel@klabbers.email>
2019-11-04 14:41:25 +0100

    from now on showing the queue sizes in the admin dashboard, expires after an hour of queue inactivity

---
+++ js/src/admin/index.js
@@ -0,0 +1,14 @@
+import app from 'flarum/app';
+import { extend } from 'flarum/extend';
+import StatusWidget from 'flarum/components/StatusWidget';
+
+app.initializers.add('bokt-redis', () => {
+  extend(StatusWidget.prototype, 'items', items => {
+    const loads = app.data.boktQueuesLoad;
+
+    for(let queue of app.data.boktQueuesSeen) {
+        const load = loads[queue] || null;
+        items.add('bokt-queue-size-' + queue, [<strong>Queue {queue}</strong>, <br/>, load || 'unknown']);
+    }
+  });
+});
--- src/Provides/Queue.php
+++ src/Provides/Queue.php
@@ -4,9 +4,17 @@ namespace Bokt\Redis\Provides;
 
 use Bokt\Redis\Configuration;
 use Bokt\Redis\Manager;
+use Flarum\Extend\Frontend;
+use Flarum\Extension\ExtensionManager;
+use Flarum\Frontend\Document;
+use Flarum\Settings\Event\Deserializing;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Redis\Factory;
+use Illuminate\Queue\Events\Looping;
 use Illuminate\Queue\RedisQueue;
+use Illuminate\Contracts\Queue\Queue as QueueContract;
 
 class Queue extends Provider
 {
@@ -24,5 +32,48 @@ class Queue extends Provider
 
             return $queue;
         });
+
+        /** @var ExtensionManager $extensions */
+        $extensions = $container->make(ExtensionManager::class);
+
+        (new Frontend('admin'))
+            ->js(__DIR__ . '/../../js/dist/admin.js')
+            ->content([$this, 'adminWidgetAttributes'])
+            ->extend($container, $extensions->getExtension('bokt-redis'));
+
+        /** @var Dispatcher $dispatcher */
+        $dispatcher = $container->make(Dispatcher::class);
+        $dispatcher->listen(Looping::class, [$this, 'trackQueues']);
+    }
+
+    public function adminWidgetAttributes(Document $document)
+    {
+        /** @var Store $cache */
+        $cache = app('cache.store');
+        /** @var QueueContract $queue */
+        $queue = app(QueueContract::class);
+
+        $queues = $cache->get('bokt.queue.queues-seen') ?? [];
+
+        if ($queue instanceof RedisQueue) {
+            $load = [];
+
+            foreach ($queues as $name) {
+                $load[$name] = $queue->getRedis()->connection()->llen('queues:' . $name);
+            }
+        }
+
+        $document->payload['boktQueuesSeen'] = $queues;
+        $document->payload['boktQueuesLoad'] = $load ?? null;
+    }
+
+    public function trackQueues(Looping $event)
+    {
+        /** @var Store $cache */
+        $cache = app('cache.store');
+
+        $queues = $cache->get('bokt.queue.queues-seen') ?? [];
+        $queues = array_merge($queues, [$event->queue]);
+        $cache->put('bokt.queue.queues-seen', array_unique($queues), 60);
     }
 }
