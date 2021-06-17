eab3c20d3a9f43b8e00b3f7be0df9ba086518b74
Daniel Klabbers <daniel+git@klabbers.email>
2021-06-17 21:36:50 +0200

    improve cluster handling

--- src/Provides/Queue.php
+++ src/Provides/Queue.php
@@ -3,11 +3,11 @@
 namespace Blomstra\Redis\Provides;
 
 use Blomstra\Redis\Configuration;
-use Blomstra\Redis\Manager;
 use Blomstra\Redis\Overrides\RedisManager;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Redis\Factory;
 use Illuminate\Queue\RedisQueue;
+use Illuminate\Support\Arr;
 
 class Queue extends Provider
 {
@@ -16,7 +16,14 @@ class Queue extends Provider
     public function __invoke(Configuration $configuration, Container $container)
     {
         $container->resolving(Factory::class, function (RedisManager $manager) use ($configuration) {
-            $manager->addConnection($this->connection, $configuration->toArray());
+            $config = $configuration->toArray();
+
+            // Queue does not like clusters, force the master connection:
+            if (Arr::get($config, 'options.replication') && $service = Arr::get($config, 'options.service')) {
+                $manager->addConnection($this->connection, $service);
+            } else {
+                $manager->addConnection($this->connection, $configuration->toArray());
+            }
         });
 
         $container->bind('flarum.queue.connection', function ($container) {
